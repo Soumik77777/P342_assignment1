36eb16e1d624b72c99627189ca3f1b87b686ee99
Soumik77777 <42136372+Soumik77777@users.noreply.github.com>
2020-08-07 18:53:58 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,26 @@
+//include library
+#include<iostream>
+using namespace std;
+
+//declare the main function
+int main()
+
+	{
+
+//declare variables
+	int sum=0;
+	int i=1;
+
+//program to print the sum of all numbers from 1 to 100
+
+//looping
+
+	while(i<=100)
+		{
+			sum=sum+i;
+			i++;
+		}
+		cout<<"The summation of all natural numbers from 1 to 100 are "<<sum<<endl;
+
+return 0;
+	}
